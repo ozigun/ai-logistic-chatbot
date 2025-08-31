415b29c5c34753923673903923f46335a173b23d
M.İyice <muratiyice88@gmail.com>
2025-08-31 20:44:58 +0300

    feat: server created

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const server = express();
+
+const PORT = 3000;
+
+
+
+server.listen(PORT, function (){
+    console.log(`NAHHHHH! Server is listnening on port ${PORT}`);
+})
